104ebb650e56c7a186bc2ef4c30c73df96fed9c0
diehoHFacchinetti <diegohfacchinetti@gmail.com>
2019-08-08 07:27:46 -0300

    curso

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2> Nossa História</h2>
+        <p> Nós somos uma comunidade ativa...</p>
+        <h2>Missão e Visão</h2>
+        <p> Existimos para diponibilizar um meio eficiente e acessivel de...</p>
+    </div>
+)
